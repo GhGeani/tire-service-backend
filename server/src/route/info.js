f60e7e7622a660b62954388b8e2f04bd6baa36bf
Gherghe Geani <gherghe_geani@yahoo.com>
2019-07-30 23:38:45 +0300

    crud

---
+++ server/src/route/info.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+router.get('/items', function(req, res) {
+
+})
+
+router.get('/item', function(req, res) {
+
+})
+
+router.post('/item', function(req, res) {
+
+})
+
+router.put('/item/:id', function(req, res) {
+
+})
+
+router.delete('/item/:id', function(req, res) {
+  
+})
+
+module.exports = router;
