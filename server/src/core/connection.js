b44e685a3405710ea952bc69651ab3877eda672d
Gherghe Geani <gherghe_geani@yahoo.com>
2019-08-04 21:33:09 +0300

    small changes to connection

--- server/src/core/connection.js
+++ server/src/core/connection.js
@@ -1,6 +1,6 @@
 module.exports = connect = (mongoose, config) => {
-   mongoose.connect(`mongodb://localhost:27017/${config.database.dbname}`, (err) => {
+   mongoose.connect(`mongodb://localhost:27017/${config.database.dbname}`,{ useNewUrlParser: true }, (err) => {
    if (err) console.log(err);
-   console.log('Connected to Database');
+   console.log(`Connected to Database -> ${config.database.dbname}`);
  });
 }
