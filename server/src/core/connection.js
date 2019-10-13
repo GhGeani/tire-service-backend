d1e6c33f219f91ae22c3b3df3974c5b22ff3e1a1
Gherghe Geani <gherghe_geani@yahoo.com>
2019-10-13 16:30:17 +0300

    remove .env

--- server/src/core/connection.js
+++ server/src/core/connection.js
@@ -1,10 +1,9 @@
-require('dotenv').config();
 
 module.exports = connect = (mongoose, config) => {
   let url = `mongodb://localhost:27017/${config.database.dbname}`
-  console.log(process.env.NODE_ENV)
-  if (process.env.NODE_ENV === 'prod') {
-    url = process.env.DB_URI;
+  if (process.env.NODE_ENV === 'production') {
+    console.log(process.env.NODE_ENV)
+    url = 'mongodb+srv://admin:Robert7771@vulc-bascov-udrx1.mongodb.net/test?retryWrites=true&w=majority';
   }
   mongoose.connect(url, { useNewUrlParser: true }, (err) => {
   if (err) console.log(err);
