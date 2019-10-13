a0c7c62314dec279036389c5d9b9ae7970c736ea
Gherghe Geani <gherghe_geani@yahoo.com>
2019-10-13 18:04:34 +0300

    set database name

--- server/src/core/connection.js
+++ server/src/core/connection.js
@@ -3,7 +3,7 @@ module.exports = connect = (mongoose, config) => {
   let url = `mongodb://localhost:27017/${config.database.dbname}`
   if (process.env.NODE_ENV === 'production') {
     console.log(process.env.NODE_ENV)
-    url = 'mongodb+srv://admin:Robert7771@vulc-bascov-udrx1.mongodb.net/test?retryWrites=true';
+    url = 'mongodb+srv://admin:Robert7771@vulc-bascov-udrx1.mongodb.net/body_shop?retryWrites=true';
   }
   mongoose.connect(url, { useNewUrlParser: true }, (err) => {
   if (err) console.log(err);
