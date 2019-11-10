2eb153effcd2bdf761d6be6a16f184db5a5459ac
Gherghe Geani <gherghe_geani@yahoo.com>
2019-11-10 14:19:17 +0200

    fixing upload

--- server/src/utils/storage.js
+++ server/src/utils/storage.js
@@ -1,6 +1,4 @@
 const cloudinary = require('cloudinary').v2;
-const cloudinaryStorage = require('multer-storage-cloudinary');
-const multer = require('multer');
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -11,12 +9,18 @@ cloudinary.config({
 
 const upload =  (file) => {
   console.log(file)
-  return cloudinary.uploader.upload(file, function(error, result) {console.log(result, error)});
+  // return cloudinary.uploader.upload(file, function(error, result) {console.log(result, error)});
+  cloudinary.uploader.upload(file, {
+    public_id: file.originalname
+  }, function(error, result) {
+    console.log(result);
+  });
+
 }
 
 const remove = (key) => {
   console.log(cloudinary);
-  return cloudinary.uploader.destroy(key, function(error) { console.log(errors) });
+  cloudinary.uploader.destroy(key, function(error) { console.log(errors) });
 };
 
 
