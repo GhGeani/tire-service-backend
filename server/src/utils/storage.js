74e35724ed9655ddcc3a1892372c0e1e437946fd
Gherghe Geani <gherghe_geani@yahoo.com>
2019-11-10 02:36:11 +0200

    change cloud to cloudinary

--- server/src/utils/storage.js
+++ server/src/utils/storage.js
@@ -1,30 +1,26 @@
-const AWS = require( 'aws-sdk' );
+const cloudinary = require('cloudinary').v2;
+const cloudinaryStorage = require('multer-storage-cloudinary');
 const multer = require('multer');
-const multerS3 = require('multer-s3');
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME || 'demsdreams',
+  api_key: process.env.API_KEY || '117631267167635',
+  api_secret: process.env.API_SECRET || 'c3b7QHsGzMpelRX3vd48-AF8Fxw'
+})
+
+const storage = cloudinaryStorage({
+  cloudinary,
+  folder: 'tireshop',
+  filename: function (req, file, cb) {
+    cb(undefined, file.originalname);
+  }
 });
 
-const s3 = new AWS.S3();
-
-const upload =  multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.S3_BUCKET_NAME,
-    key: function (req, file, cb) {
-      cb(null, file.originalname)
-    },
-
-  })
-}).any() 
+const upload =  multer({ storage }).any() 
 
 const remove = (key) => {
-  s3.deleteObject({
-    Bucket: process.env.S3_BUCKET_NAME,
-    Key: key
-  }, function(err, data){})
-}
- 
-module.exports = { upload, remove }
+  return cloudinary.uploader.destroy(key, function(result) { console.log(result) });
+};
+
+
+module.exports = { upload, remove };
